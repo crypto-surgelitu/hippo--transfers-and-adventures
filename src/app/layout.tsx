636ae9620295cf2a10768fa7f6eaca742d86c202
DonKing <anthonymuhati52@gmail.com>
2026-05-23 18:40:58 +0300

    replaced favicon with premium geometric hippo icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,7 +72,6 @@ export default function RootLayout({
     <html lang="en" className={`light ${plusJakartaSans.variable} ${notoSerif.variable}`}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </head>
       <body className="bg-background text-on-background font-body antialiased min-h-screen flex flex-col">
         <Navbar />
